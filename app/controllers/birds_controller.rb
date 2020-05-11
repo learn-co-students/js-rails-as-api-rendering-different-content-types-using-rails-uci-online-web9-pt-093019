7fa6c1201f0c075f3665a42bc8261f1f14102876
ACAPP-dev <acapp909@gmail.com>
2020-05-11 14:10:56 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,10 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    # render 'birds/index.html.erb'
+    # render plain: "Hello #{@birds[3].name}"
+    # render json: {message: "This is a json message"}
+    # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'.to_json
+    render json: birds
   end
 end
